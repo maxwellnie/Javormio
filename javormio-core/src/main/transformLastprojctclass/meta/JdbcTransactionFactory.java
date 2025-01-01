e39967bdf7a191ad5d3ace0ce99d38d9c5fb58c6
七色 <1461608744@qq.com>
2025-01-01 21:54:43 +0800

    将其他的主要文件提交，其中不包含proxy代理类文件

---
+++ javormio-core/src/main/transformLastprojctclass/meta/JdbcTransactionFactory.java
@@ -0,0 +1,45 @@
+package com.maxwellnie.velox.sql.core.natives.database.transaction.impl.jdbc;
+
+import com.maxwellnie.velox.sql.core.natives.exception.MethodNotSupportException;
+import com.maxwellnie.velox.sql.core.natives.database.transaction.Transaction;
+import com.maxwellnie.velox.sql.core.natives.database.transaction.impl.base.BaseTransactionFactory;
+import com.maxwellnie.velox.sql.core.natives.database.transaction.impl.base.ProxyCurrentDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.util.Map;
+
+/**
+ * @author Maxwell Nie
+ */
+public class JdbcTransactionFactory extends BaseTransactionFactory {
+    public JdbcTransactionFactory(DataSource dataSource) {
+        super(dataSource);
+    }
+
+    @Override
+    public Transaction produce() {
+        throw new MethodNotSupportException("This method is applied to a custom Transaction factory, the built-in factory does not support this method.");
+    }
+
+    @Override
+    public Transaction produce(boolean autoCommit, int level) {
+        return new JdbcTransaction(new ProxyCurrentDataSource(this.getDefaultDataSource()), autoCommit, level);
+    }
+
+    @Override
+    public Transaction produce(Connection connection) {
+        return new JdbcTransaction(connection);
+    }
+
+    @Override
+    public Transaction produce(Map<?, ?> map) {
+        throw new MethodNotSupportException("This method is applied to a custom Transaction factory, the built-in factory does not support this method.");
+    }
+
+    @Override
+    public Transaction produce(Object[] param) {
+        throw new MethodNotSupportException("This method is applied to a custom Transaction factory, the built-in factory does not support this method.");
+    }
+
+}
---
+++ javormio-core/src/main/transformLastprojctclass/meta/TransactionFactory.java
@@ -0,0 +1,31 @@
+package com.maxwellnie.velox.sql.core.natives.database.transaction;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.util.Map;
+
+/**
+ * 事务工厂
+ *
+ * @author Maxwell Nie
+ */
+public interface TransactionFactory {
+    Transaction produce();
+
+    /**
+     * it's mainly method,must be implemented.
+     *
+     * @param autoCommit
+     * @param level
+     * @return Transaction Object
+     */
+    Transaction produce(boolean autoCommit, int level);
+
+    Transaction produce(Connection connection);
+
+    Transaction produce(Map<?, ?> map);
+
+    Transaction produce(Object[] param);
+
+    DataSource getDefaultDataSource();
+}
