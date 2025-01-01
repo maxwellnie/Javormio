dc9b0bff766e7c634b3f164d75707462c1615335
Maxwell Nie <maxwellnie@qq.com>
2025-01-01 22:26:19 +0800

    增加注释

--- javormio-core/src/main/java/io/github/maxwellnie/javormio/core/database/sql/method/SqlMethod.java
+++ javormio-core/src/main/java/io/github/maxwellnie/javormio/core/database/sql/method/SqlMethod.java
@@ -1,9 +1,20 @@
 package io.github.maxwellnie.javormio.core.database.sql.method;
 
 /**
+ * 可对数据库进行操作的方法
  * @author Maxwell Nie
  */
 public interface SqlMethod {
+    /**
+     * 对数据库进行操作，需要精确的参数
+     * @param args
+     * @return 执行结果
+     */
     Object invokeExactly(Object... args);
+    /**
+     * 对数据库进行操作，不需要精确的参数
+     * @param args
+     * @return 执行结果
+     */
     Object invoke(Object[] args);
 }
