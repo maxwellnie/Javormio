e5f65c3644eacf43d5cfa4b5414ceb661e78e070
Maxwell Nie <maxwellnie@qq.com>
2025-01-03 23:42:49 +0800

    提交批处理查询语句执行器代码

--- javormio-core/src/main/java/io/github/maxwellnie/javormio/core/database/result/ResultSetConvertor.java
+++ javormio-core/src/main/java/io/github/maxwellnie/javormio/core/database/result/ResultSetConvertor.java
@@ -1,6 +1,7 @@
 package io.github.maxwellnie.javormio.core.database.result;
 
 import java.sql.ResultSet;
+import java.util.List;
 
 /**
  * 将ResultSet转换为Java实体对象
@@ -9,6 +10,9 @@ import java.sql.ResultSet;
  */
 public class ResultSetConvertor {
     public Object convert(ResultSet resultSet, TypeMapping typeMapping) {
+        return null;
+    }
+    public Object convert(List<ResultSet> resultSet, TypeMapping typeMapping) {
         return null;
     }
 }
