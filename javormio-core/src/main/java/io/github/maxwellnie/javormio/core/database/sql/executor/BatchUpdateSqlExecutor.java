145584e33113d79e1cd5e801400bb4e14fa46d59
七色 <1461608744@qq.com>
2025-01-04 22:28:48 +0800

    批处理提交第二次提交，删除了自动事务的错误，增加了返回的主键，修改了一堆bug

--- javormio-core/src/main/java/io/github/maxwellnie/javormio/core/database/sql/executor/BatchUpdateSqlExecutor.java
+++ javormio-core/src/main/java/io/github/maxwellnie/javormio/core/database/sql/executor/BatchUpdateSqlExecutor.java
@@ -7,6 +7,7 @@ import io.github.maxwellnie.javormio.core.database.sql.SqlType;
 import io.github.maxwellnie.javormio.core.java.api.Constants;
 
 import java.sql.*;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
@@ -28,49 +29,41 @@ public class BatchUpdateSqlExecutor extends BaseSqlExecutor {
         boolean selectGeneratedKeys = executableSql.getType().equals(SqlType.INSERT) && consumer!= null;
         //获取连接对象
         Connection connection = connectionResource.getConnection();
-        connection.setAutoCommit(false);
-      /*  connection.commit();//开始事务*/
 
-        try{
-            for (int i = 0; i <executableSql.getSqlList().length; i++) {
-                PreparedStatement preparedStatement = selectGeneratedKeys ? connection.prepareStatement(executableSql.getSqlList()[i], Statement.RETURN_GENERATED_KEYS)
-                        : connection.prepareStatement(executableSql.getSqlList()[i]);
+
+        try(PreparedStatement preparedStatement = selectGeneratedKeys ? connection.prepareStatement(executableSql.getSqlList()[0], Statement.RETURN_GENERATED_KEYS)
+                : connection.prepareStatement(executableSql.getSqlList()[0])){
                 //获取参数列表
                 List<SqlParameter[]> sqlParametersList = executableSql.getParametersList();
                 //当前行SQL的参数数组
-                SqlParameter[] sqlParameters = null;
+//                SqlParameter[] sqlParameters = null;
                 //判断参数列表是否为空，非空则设置当前行SQL参数数组
-                if (!sqlParametersList.isEmpty())
-                    sqlParameters = sqlParametersList.get(0);
-                //判断参数数组是否为空，如果不空则设置参数到报表，例如:SELECT * FROM tb_user WHERE id = ?; preparedStatement.setInt(1, 1);
-                if (sqlParameters != null) {
-                    int index = 1;
-                    //遍历参数
-                    for (SqlParameter sqlParameter : sqlParameters) {
-                        //获取参数的类型处理器，使用类型处理器设置报表的参数
-                        sqlParameter.getTypeHandler().setValue(preparedStatement, index++, sqlParameter.getValue());
+                if (!sqlParametersList.isEmpty()){
+                    for(SqlParameter[] sqlParameters : sqlParametersList){
+                        if (sqlParameters != null) {
+                            int index = 1;
+                            //遍历参数
+                            for (SqlParameter sqlParameter : sqlParameters) {
+                                //获取参数的类型处理器，使用类型处理器设置报表的参数
+                                //判断参数数组是否为空，如果不空则设置参数到报表，例如:SELECT * FROM tb_user WHERE id = ?; preparedStatement.setInt(1, 1);
+                                sqlParameter.getTypeHandler().setValue(preparedStatement, index++, sqlParameter.getValue());
+                            }
+                        }
+                        //添加批处理
+                        preparedStatement.addBatch();
                     }
                 }
-                //添加批处理
-                preparedStatement.addBatch();
-                if (i%100 == 0){
-                    //该ExecutableSql对数据库表产生的影响
-                    preparedStatement.executeBatch();
-                } else if (i==executableSql.getSqlList().length-1) {
-                    preparedStatement.executeBatch();
+                //提交到数据库
+            int[] updateCount = preparedStatement.executeBatch();
+            //判断是否需要获取生成的主键值，如果需要则交给消费者处理
+            LinkedList<ResultSet> list = new LinkedList<>();
+            for(int flag : updateCount){
+                if (flag != Statement.EXECUTE_FAILED&&selectGeneratedKeys){
+                    consumer.accept(preparedStatement.getGeneratedKeys());
                 }
-/*                //判断是否需要获取生成的主键值，如果需要则交给消费者处理
-                if (selectGeneratedKeys)
-                    consumer.accept(preparedStatement.getGeneratedKeys());*/
-
             }
-        }
-        catch(SQLException e){
-            e.printStackTrace();
-            connection.rollback();
-        }finally{
             connection.close();
+            return updateCount;
         }
-        return null;
     }
 }
