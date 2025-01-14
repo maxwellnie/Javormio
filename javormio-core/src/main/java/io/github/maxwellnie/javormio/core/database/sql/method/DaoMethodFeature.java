cdd88c4d8d3c225232985307a5b7e6ef44bf64c9
maxwellnie <2091916141@qq.com>
2025-01-14 21:48:34 +0800

    小修改

--- javormio-core/src/main/java/io/github/maxwellnie/javormio/core/database/sql/method/DaoMethodFeature.java
+++ javormio-core/src/main/java/io/github/maxwellnie/javormio/core/database/sql/method/DaoMethodFeature.java
@@ -14,12 +14,18 @@ public class DaoMethodFeature extends MethodFeature {
      * 类型映射
      */
     final TypeMapping typeMapping;
-    public DaoMethodFeature(Method method, TypeMapping typeMapping) {
+    final boolean multipleTable;
+    public DaoMethodFeature(Method method, TypeMapping typeMapping, boolean multipleTable) {
         super(method);
         this.typeMapping = typeMapping;
+        this.multipleTable = multipleTable;
     }
 
     public TypeMapping getTypeMapping() {
         return typeMapping;
+    }
+
+    public boolean isMultipleTable() {
+        return multipleTable;
     }
 }
