52471fcf1e1e242b828141c6e73ca62c2b6e16d7
七色 <1461608744@qq.com>
2025-01-17 23:24:44 +0800

    添加了简单类型的通用方法，使用的是java的反射，下一次将其更改为项目内反射，添加了自动抛出异常的注释，，下一次优化simpleConvert代码

--- javormio-core/src/main/java/io/github/maxwellnie/javormio/core/database/result/text.java
+++ javormio-core/src/main/java/io/github/maxwellnie/javormio/core/database/result/text.java
@@ -1,8 +1,14 @@
 package io.github.maxwellnie.javormio.core.database.result;
 
 import com.maxwellnie.velox.sql.core.natives.database.mapping.returntype.ReturnTypeMapping;
+import io.github.maxwellnie.javormio.core.java.reflect.Reflection;
+import io.github.maxwellnie.javormio.core.utils.ReflectionUtils;
 
+import java.lang.reflect.Field;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,6 +19,7 @@ import java.util.List;
 public class text implements ResultSetConvertor{
     method stack1 = new method<>();
     method stack2 = new method<>();
+    @lombok.SneakyThrows
     @Override
     public Object convert(ResultSet resultSet, TypeMapping typeMapping, boolean multipleTable) {
 
@@ -38,11 +45,33 @@ public class text implements ResultSetConvertor{
      * @param TypeMapping 返回类型映射，用于指导如何将数据库数据映射到业务对象
      * @return 返回转换后的对象，如果转换过程中遇到错误或者无法转换，则返回null或者空对象
      */
-    public Object simpleConvert(ResultSet resultSet, TypeMapping TypeMapping)
-    {
+    @lombok.SneakyThrows
+    public Object simpleConvert(ResultSet resultSet, TypeMapping TypeMapping){
         Class<?> mappingType = TypeMapping.getType();
-        return null;
-    }
 
+        List<Object> resultList = new ArrayList<>();
+        ResultSetMetaData metaData = resultSet.getMetaData();
+        int columnCount = metaData.getColumnCount();
+        while (!resultSet.isClosed() && resultSet.next()) {
+//            Reflection<?> reflection = ReflectionUtils.getReflection(mappingType);
+            Object instance = mappingType.newInstance();
+            for (int i = 1; i <= columnCount; i++){
+                String columnName = metaData.getColumnName(i);
+                Object columnValue = resultSet.getObject(i);
+                try {
+                    Field field = mappingType.getDeclaredField(columnName);
+                    field.setAccessible(true);
+                    field.set(instance, columnValue);
+                } catch (NoSuchFieldException e) {
+                    // 如果字段不存在，可以忽略或记录日志
+                    System.out.println("Field " + columnName + " not found in class " + mappingType.getName());
+                }
+                resultList.add(instance);
+            }
+
+        }
+
+        return resultList;
+    }
 
 }
