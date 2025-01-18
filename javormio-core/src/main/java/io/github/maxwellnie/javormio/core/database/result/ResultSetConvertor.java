2005b2339bf3e1349bbc4694e1414afa64f792fb
maxwellnie <2091916141@qq.com>
2025-01-18 13:45:53 +0800

    设定异常

--- javormio-core/src/main/java/io/github/maxwellnie/javormio/core/database/result/ResultSetConvertor.java
+++ javormio-core/src/main/java/io/github/maxwellnie/javormio/core/database/result/ResultSetConvertor.java
@@ -1,6 +1,7 @@
 package io.github.maxwellnie.javormio.core.database.result;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 /**
@@ -14,12 +15,12 @@ public interface ResultSetConvertor {
      * @param typeMapping
      * @return Object
      */
-    Object convert(ResultSet resultSet, TypeMapping typeMapping, boolean multipleTable);
+    Object convert(ResultSet resultSet, TypeMapping typeMapping, boolean multipleTable) throws ConvertException;
     /**
      * 将ResultSet列表转换为Java实体对象
      * @param resultSet
      * @param typeMapping
      * @return Object
      */
-    Object convert(List<ResultSet> resultSet, TypeMapping typeMapping, boolean multipleTable);
+    Object convert(List<ResultSet> resultSet, TypeMapping typeMapping, boolean multipleTable) throws ConvertException;
 }
