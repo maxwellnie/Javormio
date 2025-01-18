3b68982d19b2a111028ecbe13212a3a5553ac9e7
maxwellnie <2091916141@qq.com>
2025-01-18 13:45:39 +0800

    设定异常

---
+++ javormio-core/src/main/java/io/github/maxwellnie/javormio/core/database/result/ConvertException.java
@@ -0,0 +1,25 @@
+package io.github.maxwellnie.javormio.core.database.result;
+
+/**
+ * @author Maxwell Nie
+ */
+public class ConvertException extends Exception{
+    public ConvertException() {
+    }
+
+    public ConvertException(String message) {
+        super(message);
+    }
+
+    public ConvertException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ConvertException(Throwable cause) {
+        super(cause);
+    }
+
+    public ConvertException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
