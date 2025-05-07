61224f16e4685d44627244785fb2b3d261fc6e99
maxwellnie <2091916141@qq.com>
2025-05-07 12:40:49 +0800

    允许同一注解拥有多个专属处理器

--- javormio-spi/src/main/java/io/github/maxwellnie/javormio/CoreProcessor.java
+++ javormio-spi/src/main/java/io/github/maxwellnie/javormio/CoreProcessor.java
@@ -2,32 +2,34 @@ package io.github.maxwellnie.javormio;
 
 import org.apache.velocity.app.Velocity;
 
-import javax.annotation.processing.*;
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
 import java.io.InputStream;
 import java.util.*;
-import java.util.stream.Collectors;
 
 
 /**
  * @author Maxwell Nie
  */
 public class CoreProcessor extends AbstractProcessor {
-    private final Map<String, ElementHandler> typeElementHandlers;
+    private final Map<String, List<ElementHandler>> elementHandlersMap;
     private final Set<String> supportedAnnotationTypes;
     private final Set<String> supportedOptions;
     private SourceVersion supportedSourceVersion;
     private Configuration configuration;
 
     public CoreProcessor() {
-        typeElementHandlers = new HashMap<>();
-        typeElementHandlers.put("io.github.maxwellnie.javormio.common.annotation.table.Table", new MetaTableHandler());
+        elementHandlersMap = new HashMap<>();
+        List<ElementHandler> tableHandlers = new LinkedList<>();
+        tableHandlers.add(new MetaTableHandler());
+        elementHandlersMap.put("io.github.maxwellnie.javormio.common.annotation.table.Table", tableHandlers);
         // typeElementHandlers.put("io.github.maxwellnie.javormio.common.annotation.proxy.ProxyDefine", new ProxyHandler());
         supportedOptions = new HashSet<>();
         supportedAnnotationTypes = new HashSet<>();
-        supportedAnnotationTypes.addAll(typeElementHandlers.keySet());
+        supportedAnnotationTypes.addAll(elementHandlersMap.keySet());
         supportedSourceVersion = SourceVersion.RELEASE_8;
         loadConfigurations();
         initVelocity();
@@ -37,42 +39,62 @@ public class CoreProcessor extends AbstractProcessor {
      * 初始化velocity
      */
     private void initVelocity() {
-        Properties properties = configuration == null? null : configuration.getVelocityProperties();
+        Properties properties = configuration == null ? null : configuration.getVelocityProperties();
         if (properties == null) {
             properties = new Properties();
             properties.put("file.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
         }
         Velocity.init(properties);
     }
+
     /**
      * 加载配置
      */
     private void loadConfigurations() {
-        try(InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("CoreProcessor.properties")){
+        try (InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("CoreProcessor.properties")) {
             Properties properties = new Properties();
             properties.load(inputStream);
-            String className = properties.getProperty("configuration-class");
-            if (className != null){
-                Class<?> clazz = Class.forName(className);
-                if (Configuration.class.isAssignableFrom(clazz)){
-                    configuration = (Configuration) clazz.getConstructor().newInstance();
-                    Optional.of(configuration.getElementHandlers()).ifPresent(typeElementHandlers::putAll);
-                    Optional.of(configuration.getSupportedOptions()).ifPresent(supportedOptions::addAll);
-                    Optional.of(configuration.getSupportedSourceVersion()).ifPresent(sourceVersion -> supportedSourceVersion = sourceVersion);
-                    Optional.of(configuration.getElementHandlers()).ifPresent(map -> supportedAnnotationTypes.addAll(map.keySet()));
+            String classNameStr = properties.getProperty("configuration-class");
+            String[] classNames = classNameStr == null ? null : classNameStr.split(",");
+            if (classNames != null) {
+                for (String className : classNames) {
+                    if (className != null) {
+                        Class<?> clazz = Class.forName(className);
+                        if (Configuration.class.isAssignableFrom(clazz)) {
+                            configuration = (Configuration) clazz.getConstructor().newInstance();
+                            Optional.of(configuration.getElementHandlersMap())
+                                    .ifPresent(m->{
+                                        for(Map.Entry<String, List<ElementHandler>> entry: m.entrySet()){
+                                            List<ElementHandler> elementHandlers = elementHandlersMap.get(entry.getKey());
+                                            if (elementHandlers != null)
+                                                elementHandlers.addAll(entry.getValue());
+                                            else
+                                                elementHandlersMap.put(entry.getKey(), entry.getValue());
+                                        }
+                                    });
+                            Optional.of(configuration.getSupportedOptions())
+                                    .ifPresent(supportedOptions::addAll);
+                            Optional.of(configuration.getSupportedSourceVersion())
+                                    .ifPresent(sourceVersion -> supportedSourceVersion = sourceVersion);
+                            Optional.of(configuration.getElementHandlersMap())
+                                    .ifPresent(map -> supportedAnnotationTypes.addAll(map.keySet()));
+                        }
+                    }
                 }
             }
-        }catch (Exception ignored){
+        } catch (Exception ignored) {
 
         }
     }
+
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        for (TypeElement annotation : annotations){
-            ElementHandler elementHandler = typeElementHandlers.get(annotation.getQualifiedName().toString());
-            if (elementHandler != null){
-                elementHandler.handle(roundEnv.getElementsAnnotatedWith(annotation), processingEnv, roundEnv);
-            }else {
+        for (TypeElement annotation : annotations) {
+            List<ElementHandler> elementHandlers = this.elementHandlersMap.get(annotation.getQualifiedName().toString());
+            if (elementHandlers != null) {
+                for (ElementHandler elementHandler : elementHandlers)
+                    elementHandler.handle(roundEnv.getElementsAnnotatedWith(annotation), processingEnv, roundEnv);
+            } else {
                 processingEnv.getMessager().printMessage(Diagnostic.Kind.WARNING,
                         "No handler for annotation " + annotation.getQualifiedName(), annotation);
             }
