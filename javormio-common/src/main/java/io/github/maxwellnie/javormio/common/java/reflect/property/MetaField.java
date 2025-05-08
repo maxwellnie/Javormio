f7e41a4a1bc1f1ff94bd76e4c6fb4de185c54f8a
maxwellnie <2091916141@qq.com>
2025-05-08 16:54:08 +0800

    修改了MetaField的对象结构，使得其更合理

--- javormio-common/src/main/java/io/github/maxwellnie/javormio/common/java/reflect/property/MetaField.java
+++ javormio-common/src/main/java/io/github/maxwellnie/javormio/common/java/reflect/property/MetaField.java
@@ -2,54 +2,46 @@ package io.github.maxwellnie.javormio.common.java.reflect.property;
 
 import io.github.maxwellnie.javormio.common.java.proxy.MethodInvocationException;
 import io.github.maxwellnie.javormio.common.java.proxy.invocation.MethodInvoker;
-import io.github.maxwellnie.javormio.common.utils.TypeUtils;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 
 /**
- * 相对安全的对象中的字段进行赋值和查询，避免破坏封装性。
- *
  * @author Maxwell Nie
  */
-public class MetaField {
-    public static final int IS_COLLECTION = 1;
-    public static final int IS_ARRAY = 2;
-    public static final int IS_MAP = 3;
-    public static final int IS_COMPLEX = 4;
-    Field field;
-    MethodInvoker getter;
-    MethodInvoker setter;
-    int typeMask;
+public class MetaField<E, T> {
+    private final String name;
+    private final Class<T> type;
+    private final Class<E> declaringClass;
+    private final MethodInvoker<E, T> getter;
+    private final MethodInvoker<E, Object> setter;
 
-    public MetaField(Field field, MethodInvoker getter, MethodInvoker setter) {
-        this.field = field;
+    public MetaField(String name, Class<T> type, Class<E> declaringClass, MethodInvoker<E,  T> getter, MethodInvoker<E, Object> setter) {
+        this.name = name;
+        this.type = type;
+        this.declaringClass = declaringClass;
         this.getter = getter;
         this.setter = setter;
-        if (TypeUtils.isCollection(field.getType()))
-            this.typeMask &= 1;
-        if (TypeUtils.isArray(field.getType()))
-            this.typeMask &= 2;
-        if (TypeUtils.isMap(field.getType()))
-            this.typeMask &= 3;
-        if (TypeUtils.isComplexType(field.getType()))
-            this.typeMask &= 4;
     }
-
+    @SuppressWarnings("unchecked")
+    public MetaField(Field field, MethodInvoker<E,  T> getter, MethodInvoker<E, Object> setter) {
+        this.name = field.getName();
+        this.type = (Class<T>) field.getType();
+        this.declaringClass = (Class<E>) field.getDeclaringClass();
+        this.getter = getter;
+        this.setter = setter;
+    }
     /**
      * 设置对象的属性值
      *
      * @param bean
      * @param value
-     * @throws InvocationTargetException
-     * @throws IllegalAccessException
-     * @throws NoSuchMethodException
+     * @throws MethodInvocationException
      */
-    public void set(Object bean, Object value) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException, MethodInvocationException {
+    public void set(E bean, T value) throws MethodInvocationException{
         if (setter == null) {
-            throw new NoSuchMethodException("setter not found for " + field.getName());
+            throw new MethodInvocationException("setter not found for " + name);
         }
-        setter.invoke(bean, new Object[]{value});
+        setter.invokeExactly(bean, value);
     }
 
     /**
@@ -57,62 +49,32 @@ public class MetaField {
      *
      * @param bean
      * @return
-     * @throws InvocationTargetException
-     * @throws IllegalAccessException
-     * @throws NoSuchMethodException
+     * @throws MethodInvocationException
      */
-    public Object get(Object bean) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException, MethodInvocationException {
+    public Object get(E bean) throws MethodInvocationException {
         if (getter == null) {
-            throw new NoSuchMethodException("getter not found for " + field.getName());
+            throw new MethodInvocationException("getter not found for " + name);
         }
-        return getter.invoke(bean, null);
+        return getter.invoke(bean);
     }
 
-    /**
-     * 是否是集合
-     *
-     * @return
-     */
-    public boolean isCollection() {
-        return (this.typeMask & IS_COLLECTION) != 0;
-    }
-
-    /**
-     * 获取字段对象
-     *
-     * @return
-     */
-    public Field getField() {
-        return field;
-    }
-
-    /**
-     * 获取字段名
-     *
-     * @return
-     */
     public String getName() {
-        return field.getName();
+        return name;
     }
 
-    /**
-     * 获取字段类型
-     *
-     * @return
-     */
     public Class<?> getType() {
-        return field.getType();
+        return type;
     }
 
-    public boolean isArray() {
-        return (this.typeMask & IS_ARRAY) != 0;
+    public Class<?> getDeclaringClass() {
+        return declaringClass;
     }
 
-    public boolean isMap() {
-        return (this.typeMask & IS_MAP) != 0;
+    public MethodInvoker<E, T> getGetter() {
+        return getter;
     }
 
-    public boolean isComplex() {
-        return (this.typeMask & IS_COMPLEX) != 0;
+    public MethodInvoker<E, Object> getSetter() {
+        return setter;
     }
 }
