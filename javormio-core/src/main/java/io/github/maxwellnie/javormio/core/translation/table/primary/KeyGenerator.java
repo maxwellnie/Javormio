c49555fee7a0768db1ae503cf7da74b1b9d4bb62
maxwellnie <2091916141@qq.com>
2025-05-20 16:40:58 +0800

    修改了主键生成器的结构，使其适应新SqlExecutor

--- javormio-core/src/main/java/io/github/maxwellnie/javormio/core/translation/table/primary/KeyGenerator.java
+++ javormio-core/src/main/java/io/github/maxwellnie/javormio/core/translation/table/primary/KeyGenerator.java
@@ -2,24 +2,53 @@ package io.github.maxwellnie.javormio.core.translation.table.primary;
 
 import io.github.maxwellnie.javormio.common.annotation.document.ExtensionPoint;
 
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Collection;
+import java.util.List;
+
 /**
  * 主键生成器
  *
  * @author Maxwell Nie
  */
 @ExtensionPoint
-public interface KeyGenerator {
+public interface KeyGenerator<E, T>{
     /**
-     * 再添加数据前生成主键
-     *
-     * @param params
+     * 插入之前
+     * @param target
      */
-    void beforeInsert(Object[] params);
-
+    void beforeInsert(E target);
     /**
-     * 在添加数据后查询主键
-     *
-     * @param params
+     * 批量插入之前
+     * @param ts
      */
-    void afterInsert(Object[] params);
+    void beforeInsert(Collection<E> ts);
+    /**
+     * 插入之后
+     * @param target
+     * @param statement
+     */
+    void afterInsert(E target, Statement statement) throws SQLException;
+    /**
+     * 批量插入之后
+     * @param ts
+     * @param statement
+     */
+    void afterInsert(List<E> ts, Statement statement) throws SQLException;
+    /**
+     * 设置主键信息
+     * @param primaryInfo
+     * */
+    void setPrimaryInfo(PrimaryInfo<E, T> primaryInfo);
+    /**
+     * 获取表信息
+     * @return PrimaryInfo
+     * */
+    PrimaryInfo<E, T> getPrimaryInfo();
+    /**
+     * 是否接受自动生成主键
+     * @return boolean
+     * */
+    boolean isAcceptGenerateKeys();
 }
